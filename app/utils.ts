beb21df70c16875a79fa9445487a4a2109ad9c23
cennne <34563979+cennne@users.noreply.github.com>
2024-08-24 09:43:00 +0700

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -263,7 +263,7 @@ export function isVisionModel(model: string) {
     model.includes("gpt-4-turbo") && !model.includes("preview");
 
   return (
-    visionKeywords.some((keyword) => model.includes(keyword)) || isGpt4Turbo
+    visionKeywords.some((keyword) => model.includes(keyword))
   );
 }
 
